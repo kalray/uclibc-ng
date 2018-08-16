937adfd7823cf59197356e35d4b9032334629438
Clement Leger <clement.leger@kalray.eu>
2018-08-16 18:08:25 +0200

    k1c: Implement setjmp/longjmp

--- libc/sysdeps/linux/k1c/sysdep.h
+++ libc/sysdeps/linux/k1c/sysdep.h
@@ -1,5 +1,5 @@
 /*
- * This file is subject to the terms and conditions of the GNU General Public
+ * This file is subject to the terms and conditions of the LGPV V2.1
  * License.  See the file "COPYING" in the main directory of this archive
  * for more details.
  *
@@ -12,5 +12,33 @@
 #include <common/sysdep.h>
 
 #define SYS_ify(syscall_name)  (__NR_##syscall_name)
+
+#ifdef	__ASSEMBLER__
+
+# define _ENTRY(name)                    \
+  .align 8;                              \
+  .globl C_SYMBOL_NAME(name);            \
+  .func  C_SYMBOL_NAME(name);            \
+  .type  C_SYMBOL_NAME(name), @function; \
+C_SYMBOL_NAME(name):		   	 \
+	        cfi_startproc;
+
+/* Define an entry point visible from C.  */
+# ifdef PIC
+# define ENTRY(name)                    \
+  .pic					\
+  _ENTRY(name)
+
+# else
+# define ENTRY(name) _ENTRY(name)
+# endif
+
+#endif
+
+#undef END
+#define END(name) \
+  cfi_endproc;        \
+  .endfunc;           \
+  .size C_SYMBOL_NAME(name), .-C_SYMBOL_NAME(name)
 
 #endif //_LINUX_K1C_SYSDEP_H
