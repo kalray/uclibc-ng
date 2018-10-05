7add0e9b4b642990c4471461d249cea058a05464
Marc Poulhiès <marc.poulhies@kalray.eu>
2018-10-05 15:40:32 +0200

    k1c: initial work for ldso support
    Started to port k1 ldso/shared support

--- ldso/ldso/k1c/dl-startup.h
+++ ldso/ldso/k1c/dl-startup.h
@@ -1,6 +1,8 @@
 /*
  * Architecture specific code used by dl-startup.c
  * Copyright (C) 2016 Waldemar Brodkorb <wbx@uclibc-ng.org>
+ * Copyright (C) 2018 Kalray Inc.
+ *
  * Ported from GNU libc
  * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
  */
@@ -21,19 +23,30 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-#error Not even close to be ready
-
 #include <features.h>
 
 __asm__("\
 .text									\n\
 .globl _start								\n\
-.type _start, %function							\n\
+.type _start, %function						\n\
 .globl _dl_start_user							\n\
-.type _dl_start_user, %function						\n\
-_start:									\n\
-    goto 0 \n								\
-    ;;\n								\
+.type _dl_start_user, %function					\n\
+_start:								\n\
+	copyd $r0 = $sp                                                 \n\
+	call	_dl_start						\n\
+	;;								\n\
+	pcrel $r1 = @gotaddr() \n					\
+	;; \n								\
+	lwz $r1 = @gotoff(_dl_skip_args)[$r1]\n			\
+	;;\n								\
+	lwz $r2 = 0[$sp]\n						\
+	;;\n								\
+	sbfw $r2 = $r1, $r2\n						\
+	addx8d $sp = $r1, $sp\n					\
+	;;\n								\
+	sd 0[$sp] = $r2\n						\
+	icall $r0\n							\
+	;;\n								\
 ");
 
 /* Get a pointer to the argv array.  On many platforms this can be just
@@ -47,6 +60,13 @@ void PERFORM_BOOTSTRAP_RELOC(ELF_RELOC *rpnt, ElfW(Addr) *reloc_addr,
 	ElfW(Addr) symbol_addr, ElfW(Addr) load_addr, ElfW(Addr) *sym)
 {
 	switch (ELF_R_TYPE(rpnt->r_info)) {
+		case R_K1_NONE:
+			break;
+		/* case R_K1_64: */
+		/* case R_K1_GLOB_DAT: */
+		/* case R_K1_JUMP_SLOT: */
+		/* 	*reloc_addr = symbol_addr + rpnt->r_addend; */
+		/* 	break; */
 		default:
 			_dl_exit(1);
 	}
