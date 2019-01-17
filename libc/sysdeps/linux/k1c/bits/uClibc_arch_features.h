2d2cae2d8d4252e25c8a8a1ef05b6484e79feec8
Guillaume Thouvenin <gthouvenin@kalray.eu>
2019-01-17 14:35:50 +0100

    k1c: enable use of syscall6() for K1
    Summary:
    Without this feature the mmap wrapper is pushing parameters on the
    stack and $r0 is crushed with the user stack pointer. For K1 all
    parameters are passed by registers so we enable this feature.
    
    Test Plan: Check that sys_mmap from the kernel side gets the correct parameters
    
    Reviewers: O52 uClibc!
    
    Differential Revision: https://phab.kalray.eu/D464

--- libc/sysdeps/linux/k1c/bits/uClibc_arch_features.h
+++ libc/sysdeps/linux/k1c/bits/uClibc_arch_features.h
@@ -10,7 +10,7 @@
 #define __UCLIBC_ABORT_INSTRUCTION__ "errop\n\t;;\n"
 
 /* can your target use syscall6() for mmap ? */
-#undef __UCLIBC_MMAP_HAS_6_ARGS__
+#define __UCLIBC_MMAP_HAS_6_ARGS__
 
 /* does your target align 64bit values in register pairs ? (32bit arches only) */
 #undef __UCLIBC_SYSCALL_ALIGN_64BIT__
